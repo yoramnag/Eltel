fdf91e799a1003ebca7f432c4d0d06a7d0a20bc3
Yoram Nagavker <yoramnag@gmail.com>
2019-03-10 18:58:24 +0200

    add some changes

---
+++ src/runGame/StartGame.java
@@ -0,0 +1,27 @@
+package runGame;
+
+
+
+import java.util.ArrayList;
+
+import gui.GameFrame;
+import gui.InitGameGUI;
+import players.Entity;
+import readFile.ReadJsonFile;
+
+public class StartGame {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Entity> entityList = new ArrayList<Entity>();
+		ReadJsonFile.readfile(entityList);
+		//System.out.println("load Json data");
+		//GameFrame gameFrame = new GameFrame("Eltel panel", 1000, 1000,entityList);
+		InitGameGUI initGameGUI = new InitGameGUI();
+		initGameGUI.initGui(entityList);
+		
+    }
+
+	
+}
+
